44bf73c634588ca578e3118b95a4d05dcc7e59b0
saipriya23 <saipriyareddy2307@gmail.com>
2018-12-06 21:40:11 +0530

    app

---
+++ OrderFood/Controllers/PageController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OrderFood.Models;
+
+namespace OrderFood.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult Create()
+        {
+            var model = new Restaurant { id = 1, name = "swagath" };
+            //return new ObjectResult (model);
+            return View(model);
+        }
+    }
+}
